286995bca0cbef7ad4a5e56ba9969d5db7455cce
Wellyson E Brito <wellysone.brito@gmail.com>
2023-05-17 12:29:46 -0300

    feat: create stripes styles

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,17 +1,19 @@
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
-      <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16 ">
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16 ">
         <div className="absolute  right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
+        <div className="absolute bottom-0 right-2 top-0  w-2 bg-stripes " />
       </div>
 
       <div className="flex flex-col p-16">
         <div className="flex flex-1 items-center justify-center">
-          <p className="w-[360px] text-center leading-relaxed">
+          <p className="w-[360px] text-center font-sans leading-relaxed">
             Você ainda não resgistrou nenhuma lembrança, comece a{' '}
             <a className="underline hover:text-gray-50" href="">
               criar agora
-            </a>{' '}
+            </a>
+            !
           </p>
         </div>
       </div>
