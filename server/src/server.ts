fe02264c4c18f01f0ddf9d06caf680278694c4d2
Wellyson E Brito <wellysone.brito@gmail.com>
2023-05-18 13:17:36 -0300

    build: added dontenv & axios

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,11 +11,12 @@ app.register(cors, {
   origin: true,
 })
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
-    port: 3000,
+    port: 3001,
   })
   .then(() => {
-    console.log('Server listening on port 3000')
+    console.log('Server listening on port 3001')
   })
